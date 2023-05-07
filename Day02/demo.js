028569caffd26e2e2c6fea8304fc53ec127ee946
sara <sara.atef001@gmail.com>
2023-05-07 14:32:04 +0300

    part2

--- Day02/demo.js
+++ Day02/demo.js
@@ -86,34 +86,34 @@ setTimeout(() => {
 
 /** js */
 
-function getProductsId() {
-  return new Promise(function (x, y) {
-    var productIds;
-    setTimeout(() => {
-      productIds = [100, 200, 300];
-      productIds ? x(productIds) : y("Error On getting Ids List");
-    }, 2000);
-  });
-}
+// function getProductsId() {
+//   return new Promise(function (x, y) {
+//     var productIds;
+//     setTimeout(() => {
+//       productIds = [100, 200, 300];
+//       productIds ? x(productIds) : y("Error On getting Ids List");
+//     }, 2000);
+//   });
+// }
 
-function getProduct(productId) {
-  return new Promise(function (resolve, reject) {
-    var product = {
-      id: productId,
-      productName: "sumsung",
-      price: "200$",
-    };
+// function getProduct(productId) {
+//   return new Promise(function (resolve, reject) {
+//     var product = {
+//       id: productId,
+//       productName: "sumsung",
+//       price: "200$",
+//     };
 
-    product ? resolve(product) : reject("Error on getting Product");
-  });
-}
+//     product ? resolve(product) : reject("Error on getting Product");
+//   });
+// }
 
-function getPrice(product) {
-  return new Promise(function (resolve, reject) {
-    var price = product.price;
-    price ? resolve(price) : reject("Error on getting price");
-  });
-}
+// function getPrice(product) {
+//   return new Promise(function (resolve, reject) {
+//     var price = product.price;
+//     price ? resolve(price) : reject("Error on getting price");
+//   });
+// }
 
 // async function execute() {
 //   try {
@@ -172,14 +172,68 @@ getProductsId()
 //   .then((data) => console.log(data))
 //   .catch((error) => console.log(error));
 
-async function execute() {
-  try {
-    let response = await fetch("https://jsonplaceholder.typicode.com/users");
-    let userData = await response.json();
-    console.log(userData);
-  } catch (error) {
-    console.log(error);
+// async function execute() {
+//   try {
+//     let response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let userData = await response.json();
+//     console.log(userData);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// execute();
+
+/**=============== Classes============== */
+
+// super class , parent class
+class Person {
+  #id;
+  constructor(id, fname = "default fname", lname = "", age = 1) {
+    this.#id = id;
+    this.fname = fname; // initalization
+    this.lname = lname;
+    this.age = age;
+  }
+  display() {
+    console.log(this.fname);
+  }
+  //
+
+  get getId() {
+    return this.#id;
+  }
+
+  set setId(value) {
+    this.#id = value;
   }
 }
 
-execute();
+var p1 = new Person(1, "ali", "ahmed");
+
+p1.setId = "123";
+console.log(p1.getId); // get
+// subclass , child
+
+// class User extends Person {
+//   constructor(fname, lname, age, password) {
+//     super(fname, lname, age);
+//     this.password = password;
+//   }
+
+//   // override
+//   display() {
+//     console.log(this.fname + " " + this.lname);
+//   }
+
+//   sayHello() {
+//     console.log("Welcome From ITI");
+//   }
+// }
+
+// let myUser = new User("ali", "ahmed", 20, "123");
+
+// myUser.display();
+// // let p1 = new Person("mona", "ahmed", 40);
+
+// p1.display();
